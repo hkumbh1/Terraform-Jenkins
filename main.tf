cd6198205982b4da662df35cb70568c7b85879c4
hkumbh1 <121787611+hkumbh1@users.noreply.github.com>
2023-02-06 02:21:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05f53c2def3a51a08" # us-west-2
+  ami           = "ami-06ee4e2261a4dc5c3" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "HRK"
