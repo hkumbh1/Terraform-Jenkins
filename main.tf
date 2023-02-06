d5e75f5e0e915cd96e4e2f4772e3484b427f53fc
hkumbh1 <121787611+hkumbh1@users.noreply.github.com>
2023-02-06 11:31:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,4 @@ resource "aws_instance" "foo" {
       Name = "HRK1"
   }
 }
-# https://registry.terraform.io/providers/hashicorp/aws/3.74.2
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version =  "4.53.0"
-    }
-  }
-}
+
