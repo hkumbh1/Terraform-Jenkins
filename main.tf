1c00c1ad913e3dfaf9a3de326cc6d878c551bb37
hkumbh1 <121787611+hkumbh1@users.noreply.github.com>
2023-02-06 12:43:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
     region = "ap-south-1"
-    profile = "default"
 }
 
 resource "aws_instance" "foo" {
@@ -11,12 +10,4 @@ resource "aws_instance" "foo" {
   }
 }
 
-terraform {
-  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"  
-    }
-}
-}
+
