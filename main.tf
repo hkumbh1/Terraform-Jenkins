7652f9fb56270fb5e31ac03a3bbad5869e821b7c
hkumbh1 <121787611+hkumbh1@users.noreply.github.com>
2023-02-06 12:29:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIA5BWOMLR5KCUG6PVO"
-     secret_key = "HKGUaOzArcPRahNteXoDvftOupFLp+vB2XN7eUG3"
+    profile = "default"
 }
 
 resource "aws_instance" "foo" {
