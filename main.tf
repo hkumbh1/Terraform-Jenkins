871de98900a17cfff81571379143e64415f1c98c
hkumbh1 <121787611+hkumbh1@users.noreply.github.com>
2023-02-06 12:04:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"  
-    }
-}
-}
-
 provider "aws" {
     region = "ap-south-1"
     access_key = "AKIA5BWOMLR5KCUG6PVO"
@@ -22,3 +12,12 @@ resource "aws_instance" "foo" {
   }
 }
 
+terraform {
+  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  
+    }
+}
+}
